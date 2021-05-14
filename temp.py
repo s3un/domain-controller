a106a9d519acdf8340534e08abc5e81552169f72
ja3y <37482108+ja3y@users.noreply.github.com>
2021-05-14 13:00:52 -0400

    Update temp.py

--- temp.py
+++ temp.py
@@ -1,10 +1,17 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from time import sleep
 
-browser = webdriver.Firefox()
-browser.get("https://python.org")
-sleep(1)
-browser.get_screenshot_as_file("new.png")
-browser.quit()
 
+WINDOW_SIZE = "1920,1080"
+options = Options()  
+options.add_argument("--headless")  
+options.add_argument("--window-size=%s" % WINDOW_SIZE)
 
+domain = input("Enter your domain$ ")
+output= input("Enter Output File Name$ ")
+driver = webdriver.Chrome(options=options)
+driver.get(domain)
+driver.set_page_load_timeout(20)
+driver.get_screenshot_as_file(output+'.png')
+print("[+] Screenshot Saved")
